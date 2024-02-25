fd74c3a945d462b57cd8953782d220a8a3f5b5e2
vijay <vj366331@gmail.com>
2024-02-25 22:05:07 +0530

    intermediate

--- LearnClasses/Program.cs
+++ LearnClasses/Program.cs
@@ -1,27 +1,34 @@
 using System;
+using LearnClasses;
 
 
-namespace Learnclasses 
+namespace LearnClasses
 {
     public class Person
-{
-    public string ? Name;
-    public void Introduce(string To)
     {
-        System.Console.WriteLine("Hi {0}, My name is {1}",To,Name);
+        public string? Name;
+        public void Introduce(string To)
+        {
+            System.Console.WriteLine("Hi {0}, My name is {1}", To, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
     }
-    public static Person Parse(string str){
-        var person = new Person();
-        person.Name = str;
-        return person;
-    }
-}
-class Program
-{
- static void Main(string[] args)
+    class Program
     {
-        var person = Person.Parse("John");
-        person.Introduce("Mosh");
+        static void Main(string[] args)
+        {
+
+            var point = new Point(10,20);
+            point.Move(new Point(40,50));
+            System.Console.WriteLine("point x is at {0},{1}",point.X,point.Y);
+            point.Move(100,200);
+            System.Console.WriteLine("point x is at {0},{1}",point.X,point.Y);
+        }
     }
-  }
+
 }
